bfb9e596ca29fad2000e6e00b9ffe8d9bd9dfa38
Jon Williams <williamsjon@johndeere.com>
2019-04-02 17:37:46 -0500

    removing bad test file and relocating the 'good' copy

--- traffic/car/tests/car/test_car.py
+++ traffic/car/tests/car/test_car.py
@@ -3,13 +3,7 @@ from car import car
 
 
 class TestCar():
-    def test__init__(self):
-        my_car = car.car()
+    def test_constructor(self):
+        my_car = car()
         assert my_car.time_waiting == 0
 
-
-    def test_update_queue_id(self):
-        my_car = car.car()
-        assert car1.queue_id == 'left side a'
-        car1.queue_id = 'left funnel'
-        assert car1.queue_id == 'left funnel'
--- traffic/car/tests/test_car.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from car import car
-
-
-class TestCar():
-    def test_constructor(self):
-        my_car = car()
-        assert my_car.time_waiting == 0
-
