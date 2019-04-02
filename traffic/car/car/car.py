378de951a5f6bb524ac9b592743c7bcd94993bae
Jon Williams <williamsjon@johndeere.com>
2019-04-02 17:35:23 -0500

    moving towards getting first unit test working

--- traffic/car/car/car.py
+++ traffic/car/car/car.py
@@ -1,8 +1,8 @@
 class car:
     'Defines a single car with its queue status and wait time'
-    def __init__(self):
+    def __init__(self, latency:int = random.randrange(1,6), queue_id:str):
         self.queue_id = queue_id
-        self.queue_position = queue_position
+        self.latency = latency
         self.time_waiting = 0
 
 
--- traffic/car/tests/test_car.py
+++ traffic/car/tests/test_car.py
@@ -3,7 +3,7 @@ from car import car
 
 
 class TestCar():
-    def test__init__(self):
+    def test_constructor(self):
         my_car = car()
         assert my_car.time_waiting == 0
 
