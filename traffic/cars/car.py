f86ca3d5a7f54c1a7371175d5805f48be3040d63
Jon Williams <williamsjon@johndeere.com>
2019-04-07 14:45:11 -0500

    refactored to make internal variables all private, added get/set methods to work with the variables, added notify() method to handle state changes, and added associated tests

--- traffic/cars/car.py
+++ traffic/cars/car.py
@@ -2,13 +2,73 @@ import random
 
 class Car:
     'Defines a single car with its queue status and wait time'
-    def __init__(self, queue_id:str, latency:int = random.randrange(1,6)):
-        self.queue_id = queue_id
-        self.latency = latency
-        self.time_waiting = 0
-        self.state = 0
-        self.distance_remaining = 0
+    def __init__(self, queue_id:str):
+        print("I was just born")
+        self.__queue_id = queue_id
+        self.__latency = 0
+        self.__time_waiting = 0
+        self.__state = 'waiting'
+        self.__distance_moved = 0
+
+
+    def set_value(self, variable: str, value: str, type: str):
+        if type == 'int':
+            my_value = int(value)
+        else:
+            my_value = value
+        if variable == 'queue_id':
+            self.__queue_id = my_value
+            return self.__queue_id
+        if variable == 'latency':
+            self.__latency = my_value
+            return self.__latency
+        if variable == 'time_waiting':
+            self.__time_waiting = my_value
+            return self.__time_waiting
+        if variable == 'state':
+            self.__state = my_value
+            return self.__state
+        if variable == 'distance_moved':
+            self.__distance_moved = my_value
+            return self.__distance_moved
 
 
     def __str__(self):
-        return 'Queue ID: {0}; Latency: {1}'.format(self.queue_id, self.latency)
+        return ('Queue ID: {0}; Latency: {1}, Moved: {2}'
+               ).format(self.__queue_id,
+                        self.__latency,
+                        self.__distance_moved
+                        )
+
+
+    def get_variables(self):
+        return {'latency': self.__latency,
+                'state': self.__state,
+                'queue_id': self.__queue_id,
+                'distance_moved': self.__distance_moved,
+                'time_waiting': self.__time_waiting
+               }
+
+
+    def notify(self, can_move: str):
+        self.__time_waiting += 1
+        if can_move:
+            if self.__state == 'waiting':
+                self.__state = 'reacting'
+                self.__latency = random.randrange(2,7)
+                self.__distance_moved = 0
+                print(("I was waiting, now I'm reacting ({0}), I've moved {1}"
+                      ).format(self.__latency, self.__distance_moved))
+            elif self.__state == 'reacting':
+                if self.__latency > 0:
+                    print("I am still reacting, latency is {0}".format(self.__latency))
+                    self.__latency -= 1
+                else:
+                    self.__state = 'moving'
+                    self.__distance_moved += 1
+                    print(("I am no longer reacting, I've moved {0}"
+                          ).format(self.__distance_moved))
+            elif self.__state == 'moving':
+                self.__distance_moved += 1
+                print("I am already moving, I've moved  {0}".format(self.__distance_moved))
+        return self.__state, self.__distance_moved
--- traffic/tests/test_car.py
+++ traffic/tests/test_car.py
@@ -1,19 +1,68 @@
 import pytest
 from cars.car import Car
+import sys
 
 class TestClass(object):
     def test_constructor(self):
         with pytest.raises(TypeError):
             my_car = Car()
         my_car = Car(queue_id = 'test queue')
-        assert my_car.queue_id == 'test queue'
-        assert my_car.latency > 0
-        assert my_car.state == 0
-        assert my_car.distance_remaining == 0
-        my_car2 = Car(queue_id = 'test queue', latency = 1)
-        assert my_car2.latency == 1
+        assert my_car.get_variables()['queue_id'] == 'test queue'
+        assert my_car.get_variables()['state'] == 'waiting'
+        assert my_car.get_variables()['distance_moved'] == 0
 
+
+    def test_private_variables(self):
+        my_car = Car(queue_id = 'private test')
+        for variable in ['my_car.latency',
+                         'my_car.state',
+                         'my_car.queue_id',
+                         'my_car.distance_moved']:
+            with pytest.raises(AttributeError):
+                eval(variable)
+
+
+    # def test_get_variables(self):
+    #     my_car = Car(queue_id = 'get_variables test')
+    #     assert my_car.get_variables()['latency'] == 0
+
+
+    def test_set_value(self):
+        my_car = Car(queue_id = 'set_value test')
+        assert my_car.set_value(variable = 'latency',
+                                value='1',
+                                type='int') == 1
+        assert my_car.set_value(variable = 'queue_id',
+                                value='set test',
+                                type='str') == 'set test'
+        assert my_car.set_value(variable = 'time_waiting',
+                                value='9',
+                                type='int') == 9
+        assert my_car.set_value(variable = 'state',
+                                value='moving',
+                                type='str') == 'moving'
+        assert my_car.set_value(variable = 'distance_moved',
+                                value='99',
+                                type='int') == 99
 
     def test_str_output(self):
-        my_car = Car(queue_id = 'str method test', latency = 3)
-        assert str(my_car) == 'Queue ID: str method test; Latency: 3'
+        my_car = Car(queue_id = 'str method test')
+        assert str(my_car) == 'Queue ID: str method test; Latency: 0, Moved: 0'
+
+
+    def test_notify(self):
+        my_car = None
+        my_car = Car(queue_id = 'notify method test')
+        assert my_car.get_variables()['time_waiting'] == 0
+        print("Test> b4 first notify()")
+        assert my_car.notify(can_move = True) == ('reacting', 0) # wait->react
+        assert my_car.get_variables()['latency'] >= 1
+        assert my_car.get_variables()['distance_moved'] == 0
+        my_car.set_value(variable = 'latency', value = '1', type = 'int')
+        print("Test> b4 2nd notify()")
+        assert my_car.notify(can_move = True) == ('reacting', 0) #
+        assert my_car.get_variables()['time_waiting'] == 2
+        print("Test> b4 3rd notify()")
+        assert my_car.notify(can_move = True) == ('moving', 1)
+        print("Test> b4 4th notify()")
+        assert my_car.notify(can_move = True) == ('moving', 2)
