3de29407095173c14bfa84725dc3ed0c7d670079
Jon Williams <williamsjon@johndeere.com>
2019-04-16 20:40:51 -0500

    refactoring test file locations to put the tests with the modules they validate

---
+++ traffic/intersections/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+from cars.fleet import Fleet
+from cars.car import Car
+from intersections.queue import Queue
+
+@pytest.fixture
+def build_fleet():
+    return Fleet(queue_id='test')
+
+
+@pytest.fixture
+def build_queue():
+    return Queue(queue_id='fixture queue')
+
+
+@pytest.fixture
+def fill_queue():
+    my_queue = Queue(queue_id = 'fixture full queue', max_queue_depth = 10)
+    for _ in range(my_queue.get_variables()['max_queue_depth']):
+        my_queue.add_car(Car(queue_id = my_queue.get_variables()['queue_id']))
+    return my_queue
