e8b84f5af5516ecb7b52bd77f047afacfc0abfe8
Jon Williams <williamsjon@johndeere.com>
2019-05-08 17:48:46 -0500

    removing vestigal fleet module that was obviated by addition of fill() method on Queue objects

--- traffic/intersections/conftest.py
+++ traffic/intersections/conftest.py
@@ -1,20 +1,21 @@
-import pytest
-from cars.fleet import Fleet
-from cars.car import Car
-from intersections.queue import Queue
+'''
+Fixtures for unit tests of intersections package
+'''
 
-@pytest.fixture
-def build_fleet():
-    return Fleet(queue_id='test')
+
+import pytest
+from intersections.queue import Queue
 
 
 @pytest.fixture
 def build_queue():
+    '''Builds an empty Queue'''
     return Queue(queue_id='fixture queue')
 
 
 @pytest.fixture
 def full_queue():
+    '''Builds a Queue that's filled with Cars'''
     my_queue = Queue(queue_id='fixture full queue', max_queue_depth=10)
     my_queue.fill()
     return my_queue
