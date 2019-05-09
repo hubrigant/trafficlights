43a00d01ced17896e6df6f5510120fe17d84d95f
Jon Williams <williamsjon@johndeere.com>
2019-05-09 09:38:28 -0500

    starting work on stoplight module

---
+++ traffic/intersections/stoplight.py
@@ -0,0 +1,61 @@
+"""
+A list of Car objects all waiting at one side of an intersection and controlled
+by one traffic light.
+
+The queue module defines a set of states and behaviors associated with a
+traffic queue. A queue represents the cars on one street of a traffic
+light-controlled intersection that are all traveling in the same direction.
+
+A Queue object is iterable
+
+A queue has several attributes:
+    - The maximum number of cars the queue can hold (__max_queue_depth variable
+      default 10)
+    - The current state (__state variable, either waiting or moving)
+    - The string identifier of the queue (__queue_id variable)
+    - The list of cars currently in the queue (__cars list)
+
+Notes
+-----
+The Queue class is implemented with encapsulated internal state. The four
+state attributes (__max_queue_depth, __queue_id, __cars, and __state) are
+private, exposed via the get_variables() and set_value() methods.
+
+"""
+
+from typing import List
+
+
+class Stoplight:
+    """
+    Define a Stoplight that controls the flow of Cars between one or more
+    Queues.
+
+
+    Parameters
+    ----------
+    queue_id : str
+        The identifier name of the queue (required).
+    max_queue_depth : int
+        The maximum number of Cars in the queue (default 10).
+
+    Attributes
+    ----------
+    __max_queue_depth : int
+        The maximum number of Cars the Queue can hold (default 10).
+    __queue_id : str
+        The string identifier of the Queue.
+    __cars : array_like
+        The list of Car instances currently in the Queue
+    __state : str
+        The current state of the Queue, either 'waiting' or 'moving'. Initial
+        state is 'waiting'.
+    """
+
+    def __init__(self, in_queues: List=None):
+        __in_queues = in_queues
+        # __out_queue = out_queue
+        pass
+
+    def num_inputs(self):
+        return len(self.__in_queues)
---
+++ traffic/intersections/test_stoplight.py
@@ -0,0 +1,20 @@
+import pytest
+# import collections
+# from cars.car import Car
+from intersections.queue import Queue
+from intersections.stoplight import Stoplight
+
+
+class TestClass(object):
+    def test_constructor(self):
+        # with pytest.raises(TypeError):
+        #     my_stoplight = Stoplight()
+        my_stoplight = Stoplight()
+        assert my_stoplight is not None
+        in_queue = Queue(queue_id='in_queue')
+        out_queue = Queue(queue_id='out_queue')
+        my_two_queue_light = Stoplight(in_queues=[in_queue],
+                                       out_queues=[out_queue])
+        assert my_two_queue_light.num_inputs == 1
+        # my_queue = Queue(queue_id="test1", max_queue_depth=10)
+        # assert my_queue.get_variables()['queue_id'] == "test1"
